e2196b4ae55c81a4e4591449b5d831609a711a1f
wandaafifah <42725331+wandaafifah@users.noreply.github.com>
2018-10-16 16:30:18 +0700

    Add files via upload

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Okt 2018 pada 05.56
--- Versi server: 10.1.34-MariaDB
--- Versi PHP: 7.2.7
+-- Generation Time: Oct 16, 2018 at 06:30 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data`
+-- Table structure for table `data`
 --
 
 CREATE TABLE `data` (
@@ -40,33 +40,35 @@ CREATE TABLE `data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `data`
+-- Dumping data for table `data`
 --
 
 INSERT INTO `data` (`id`, `nama`, `nim`, `jeniskelamin`, `prodi`, `fakultas`, `asal`, `motohidup`) VALUES
 (2, 'dis', 45, 'Laki-Laki', 'Manajemen Informatika', 'FIT', 'jakarta', 'creating the future'),
 (4, 'uti', 3436, 'Perempuan', 'DKV', 'FIT', 'jakarta', 'creating the future'),
-(5, 'uti', 3436, 'Perempuan', 'DKV', 'FIT', 'jakarta', 'creating the future');
+(5, 'uti', 3436, 'Perempuan', 'DKV', 'FIT', 'jakarta', 'creating the future'),
+(6, 'sri', 8368, 'Perempuan', 'perhotelan', 'FIT', 'bandung', 'creating the future'),
+(7, 'vgbj', 789, 'Laki-Laki', 'Manajemen Informatika', 'FIT', 'bandung', 'creating the future');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `data`
+-- Indexes for table `data`
 --
 ALTER TABLE `data`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `data`
+-- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
